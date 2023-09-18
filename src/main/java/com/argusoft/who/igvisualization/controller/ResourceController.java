b17ada73d1a9a017a9efab1504301309734cb291
HarikrashnaParikh <126066051+HarikrashnaParikh@users.noreply.github.com>
2023-09-18 14:51:26 +0530

    Update ResourceController.java

--- src/main/java/com/argusoft/who/igvisualization/controller/ResourceController.java
+++ src/main/java/com/argusoft/who/igvisualization/controller/ResourceController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class ResourceController {
